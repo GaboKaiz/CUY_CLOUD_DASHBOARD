85a02a0c9ddb40f443aec40a4350d0d2e56abbb9
GaboKaiz <xzyanzx.zz@gmail.com>
2025-10-22 10:27:18 -0500

    Mjoerado

--- cuy_db.sql
+++ cuy_db.sql
@@ -7,6 +7,8 @@
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
+
+--Nombre de la base de datos: `cuy_db`
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
